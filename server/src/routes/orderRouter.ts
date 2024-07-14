3cafddd0a3e5e0f71c8e58542c8f77cbf6421718
Hoangdebug <hoangkrt1@gmail.com>
2024-07-15 00:10:13 +0700

    code

--- server/src/routes/orderRouter.ts
+++ server/src/routes/orderRouter.ts
@@ -3,7 +3,7 @@ const router = express.Router()
 const ctrls = require('../controller/orderController')
 const { verifyAccessToken, isAdmin, isOrganizer} = require('../middlewares/verifyToken')
 
-router.post('/', ctrls.createOrder);
+router.post('/:eid',verifyAccessToken, ctrls.createOrder);
 router.get('/:id', ctrls.getOrder);
 router.put('/:id', ctrls.updateOrder); 
 router.delete('/:id', ctrls.deleteOrder);
