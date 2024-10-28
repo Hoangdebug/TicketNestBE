b2c0528947e1b42da4c9358d6098f149c824b7ed
NguyenBaoTrung080402 <taokhongpk9b@gmail.com>
2024-10-29 00:10:46 +0700

    fix code

--- server/src/models/comment.ts
+++ server/src/models/comment.ts
@@ -1,1 +1,41 @@
-//!AbortController
+const mongoose = require('mongoose');
+const moment = require('moment-timezone');
+
+const commentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    eventId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'EventModel',
+        required: true
+    },
+    parentComment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+        default: null
+    }
+}, { timestamps: true });
+
+commentSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    
+    if (obj.createdAt) {
+        obj.createdAt = moment(obj.createdAt).tz('Asia/Ho_Chi_Minh').format('YYYY/MM/DD HH:mm');
+    }
+    if (obj.updatedAt) {
+        obj.updatedAt = moment(obj.updatedAt).tz('Asia/Ho_Chi_Minh').format('YYYY/MM/DD HH:mm');
+    }
+
+    return obj;
+};
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
