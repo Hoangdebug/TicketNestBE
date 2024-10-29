43df7b7829eb9530f38064aa4952f5e226b53710
Trungnb <trungnb@vnext.vn>
2024-10-29 16:18:15 +0700

    fix comment

--- server/src/controller/commentController.ts
+++ server/src/controller/commentController.ts
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unsafe-optional-chaining */
 import { Request, Response } from 'express';
 const asyncHandler = require("express-async-handler")
 const User = require('../models/user');
 const Comment = require('../models/comment')
 
-import EventModel, { IEvent } from '~/models/event';
+import EventModel from '~/models/event';
 
 const createComment = asyncHandler(async (req: Request, res: Response) => {
     const { _id: userId } = req?.user;
@@ -47,9 +49,9 @@ const createComment = asyncHandler(async (req: Request, res: Response) => {
 
 
 const getListCommentByIdEvent = asyncHandler(async (req: Request, res: Response) => {
-    const {eid} = req.params
+    const { eid } = req.params
     const event = await EventModel.findById(eid)?.populate('created_by');
-    if(!event){
+    if (!event) {
         return res.status(404).json({
             status: false,
             code: 404,
@@ -57,8 +59,64 @@ const getListCommentByIdEvent = asyncHandler(async (req: Request, res: Response)
         });
     }
     const response = await Comment.find({ eventId: eid })
-    .populate('userId')
-    .populate('eventId');
+        .populate('userId', 'username images')
+
+    return res.status(200).json({
+        status: response ? true : false,
+        code: response ? 200 : 400,
+        message: 'Fetched comments successfully',
+        result: response
+    })
+})
+
+const replyComment = asyncHandler(async (req: Request, res: Response) => {
+    const { _id: userId } = req?.user
+    const { eid, idComment } = req.params
+
+    const user = await User?.findById(userId)
+    const event = await EventModel.findById(eid)?.populate('created_by')
+    const commentDetail = await Comment.findById(idComment)?.populate('parentComment')
+
+    if (!user || !event || !commentDetail) {
+        return res.status(404).json({
+            status: false,
+            code: 404,
+            message: 'User or event not found',
+        });
+    }
+    const { comment } = req.body
+    const response = await Comment.create({
+        userId: user._id,
+        comment,
+        eventId: event._id,
+        parentComment: commentDetail._id || null,
+    })
+
+    return res.status(200).json({
+        status: response ? true : false,
+        code: response ? 200 : 400,
+        message: 'Fetched comments successfully',
+        result: response
+    })
+})
+
+const getListCommentById = asyncHandler(async (req: Request, res: Response) => {
+    const {eid, idComment} = req.params
+    const event = await EventModel.findById(eid)?.populate('created_by')
+
+    if (!event) {
+        return res.status(404).json({
+            status: false,
+            code: 404,
+            message: 'Event not found',
+        });
+    }
+
+    const response = await Comment.find({
+        parentComment: idComment
+    })
+    .populate('userId', 'username images')
+    .populate('parentComment');
 
     return res.status(200).json({
         status: response ? true : false,
@@ -70,5 +128,7 @@ const getListCommentByIdEvent = asyncHandler(async (req: Request, res: Response)
 
 export {
     createComment,
-    getListCommentByIdEvent
+    getListCommentByIdEvent,
+    replyComment,
+    getListCommentById
 }
--- server/src/routes/commentRouter.ts
+++ server/src/routes/commentRouter.ts
@@ -5,5 +5,7 @@ const { verifyAccessToken } = require('../middlewares/verifyToken')
 
 router.post('/:eid', [verifyAccessToken], ctrls.createComment)
 router.get('/:eid', [verifyAccessToken], ctrls.getListCommentByIdEvent)
+router.post('/reply/:eid/:idComment', [verifyAccessToken], ctrls.replyComment)
+router.get('/reply/:eid/:idComment', [verifyAccessToken], ctrls.getListCommentById)
 
 module.exports = router
